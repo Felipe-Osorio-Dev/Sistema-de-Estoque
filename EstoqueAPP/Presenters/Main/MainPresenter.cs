450471861ed99953af8beb8c1e2d87ea2c84c73e
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-10 16:05:53 -0300

    feat: Criado os Presenters das views para lidar com a logica e eventos da UI.

---
+++ EstoqueAPP/Presenters/Main/MainPresenter.cs
@@ -0,0 +1,28 @@
+﻿using EstoqueAPP.Presenters.Register;
+using EstoqueAPP.Services.Navigation;
+using EstoqueAPP.Views.Main;
+using EstoqueAPP.Views.Register;
+
+namespace EstoqueAPP.Presenters.Main
+{
+    internal class MainPresenter
+    {
+        private readonly IMainView _view;
+        private readonly INavigationService _navigationService;
+
+        public MainPresenter(IMainView view, INavigationService navigationService)
+        {
+            _view = view;
+            _navigationService = navigationService;
+
+            _navigationService.SetContainerParent((Form) view);
+
+            _view.RegisterProduct += OnRegisterProduct;
+        }
+
+        private void OnRegisterProduct(Object sender, EventArgs e)
+        {
+            _navigationService.NavigateTo<RegisterForm, RegisterPresenter>();
+        }
+    }
+}
---
+++ EstoqueAPP/Presenters/Register/RegisterPresenter.cs
@@ -0,0 +1,14 @@
+﻿using EstoqueAPP.Views.Register;
+
+namespace EstoqueAPP.Presenters.Register
+{
+    internal class RegisterPresenter
+    {
+        private readonly IRegisterView _view;
+
+        public RegisterPresenter(IRegisterView view)
+        {
+            _view = view;
+        }
+    }
+}
