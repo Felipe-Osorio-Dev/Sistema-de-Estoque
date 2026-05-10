a44ab3a1d01c8b3201708c09951fbffc7aad6808
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-10 16:06:18 -0300

    feat: Criado classe para orquestrar a navegação do app.

---
+++ EstoqueAPP/Services/Navigation/INavigationService.cs
@@ -0,0 +1,8 @@
+﻿namespace EstoqueAPP.Services.Navigation
+{
+    internal interface INavigationService
+    {
+        void SetContainerParent(Form form);
+        void NavigateTo<TForm, TPresenter>() where TForm : Form where TPresenter : class;
+    }
+}
---
+++ EstoqueAPP/Services/Navigation/NavigationService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EstoqueAPP.Services.Navigation
+{
+    internal class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _services;
+        private static Form? _mdiParent;
+
+        public NavigationService(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public void NavigateTo<TForm, TPresenter>()
+            where TForm : Form
+            where TPresenter : class
+        {
+            var scoped = _services.CreateScope();
+
+            var existingForm = _mdiParent?.MdiChildren.FirstOrDefault(f => f is TForm);
+
+            if (existingForm != null)
+            {
+                existingForm.Activate();
+                scoped.Dispose();
+                return;
+            }
+
+            var newForm = scoped.ServiceProvider.GetRequiredService<TForm>();
+            scoped.ServiceProvider.GetRequiredService<TPresenter>();
+
+            newForm.FormClosed += (s, e) => scoped.Dispose();
+            newForm.MdiParent = _mdiParent;
+            newForm.Show();
+
+
+        }
+
+        public void SetContainerParent(Form form)
+        {
+            _mdiParent = form;
+        }
+    }
+}
