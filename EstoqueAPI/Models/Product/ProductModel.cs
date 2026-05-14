9613b949eee2111e56904de0a938aed03286f59a
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-14 04:43:46 -0300

    feat: Criacao da classe model/entity que representara a tabela produtos no db.

---
+++ EstoqueAPI/Models/Product/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EstoqueAPI.Models.Product
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string BarCode { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
