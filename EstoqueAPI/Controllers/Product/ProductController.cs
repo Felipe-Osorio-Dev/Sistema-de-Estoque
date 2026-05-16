3fce7d651acd0069f70f8ea5c91cd77d17754659
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-16 16:26:41 -0300

    feat: Criado classe controller para expor a api.

---
+++ EstoqueAPI/Controllers/Product/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using EstoqueAPI.DTOs.Requests;
+using EstoqueAPI.Services.Product;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EstoqueAPI.Controllers.Product
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RegisterProductDTO product)
+        {
+            await _productService.RegisterProduct(product);
+
+            return Created();
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
