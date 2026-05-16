50d04800775e50285f041b41ee5498fb8ba45cd8
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-16 16:26:12 -0300

    feat: Criado classe service para lidar com dtos e logicas complexas.

---
+++ EstoqueAPI/Services/Product/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using EstoqueAPI.DTOs.Requests;
+
+namespace EstoqueAPI.Services.Product
+{
+    public interface IProductService
+    {
+        Task RegisterProduct(RegisterProductDTO product);
+    }
+}
---
+++ EstoqueAPI/Services/Product/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using EstoqueAPI.DTOs.Requests;
+using EstoqueAPI.Models.Product;
+using EstoqueAPI.Repositorys.Product;
+using MapsterMapper;
+
+namespace EstoqueAPI.Services.Product
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task RegisterProduct(RegisterProductDTO product)
+        {
+            var model = _mapper.Map<ProductModel>(product);
+
+            await _repository.RegisterProduct(model);
+        }
+    }
+}
