3b5d0ced72f44436126f7c38fc2393b18841ded9
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-16 16:27:18 -0300

    build: Adicionado configuracao de DI do mapster e das classes de repository e service.

--- EstoqueAPI/Program.cs
+++ EstoqueAPI/Program.cs
@@ -1,4 +1,7 @@
 using EstoqueAPI.Data;
+using EstoqueAPI.Repositorys.Product;
+using EstoqueAPI.Services.Product;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMapster();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
 
