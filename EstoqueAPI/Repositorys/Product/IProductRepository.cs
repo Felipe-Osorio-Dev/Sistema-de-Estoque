b13c9bb5cf8183691b4ee39a3358035349c523bc
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-16 16:25:42 -0300

    feat: Criado classe repository para lidar diretamente com o banco de dados.

---
+++ EstoqueAPI/Repositorys/Product/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using EstoqueAPI.Models.Product;
+
+namespace EstoqueAPI.Repositorys.Product
+{
+    public interface IProductRepository
+    {
+        Task RegisterProduct(ProductModel product);
+    }
+}
---
+++ EstoqueAPI/Repositorys/Product/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using EstoqueAPI.Data;
+using EstoqueAPI.Models.Product;
+
+namespace EstoqueAPI.Repositorys.Product
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task RegisterProduct(ProductModel product)
+        {
+            await _dbContext.Produtos.AddAsync(product);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
