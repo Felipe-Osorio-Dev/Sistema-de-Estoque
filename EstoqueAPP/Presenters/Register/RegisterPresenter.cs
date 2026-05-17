86d2c8e23d208d6ef4eae9e806005153af5c1d6a
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-17 14:39:38 -0300

    feat: Implementacao dos eventos da tela de registro

--- EstoqueAPP/Presenters/Register/RegisterPresenter.cs
+++ EstoqueAPP/Presenters/Register/RegisterPresenter.cs
@@ -1,14 +1,25 @@
-﻿using EstoqueAPP.Views.Register;
+﻿using EstoqueAPP.Services.Product;
+using EstoqueAPP.Views.Register;
 
 namespace EstoqueAPP.Presenters.Register
 {
     internal class RegisterPresenter
     {
         private readonly IRegisterView _view;
+        private readonly IProductService _productService;
 
-        public RegisterPresenter(IRegisterView view)
+        public RegisterPresenter(IRegisterView view, IProductService service)
         {
             _view = view;
+            _productService = service;
+
+            _view.ClickRegisterProduct += OnRegisterProduct;
+        }
+
+        private async void OnRegisterProduct(object sender, RegisterProductEventArgs e)
+        {
+            MessageBox.Show("Testando");
+            await _productService.RegisterProduct(e.Product);
         }
     }
 }
