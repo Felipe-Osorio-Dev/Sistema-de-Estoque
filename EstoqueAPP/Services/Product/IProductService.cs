1fdc36446e47e88e0df013fb82a0fd3d9aa3a479
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-05-17 14:39:54 -0300

    feat: Criado classe service para se comunicar com a api.

---
+++ EstoqueAPP/Services/Product/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using EstoqueAPP.DTO.Requests;
+
+namespace EstoqueAPP.Services.Product
+{
+    internal interface IProductService
+    {
+        Task RegisterProduct(RegisterProductDTO product);
+    }
+}
---
+++ EstoqueAPP/Services/Product/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using EstoqueAPP.DTO.Requests;
+using System.Net.Http.Json;
+
+namespace EstoqueAPP.Services.Product
+{
+    internal class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task RegisterProduct(RegisterProductDTO product)
+        {
+            await _httpClient.PostAsJsonAsync("register", product);
+        }
+    }
+}
